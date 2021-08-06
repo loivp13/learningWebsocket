5c4c4bd4dde47baf981fd4adbd6ca3e891f6e40f
Loi <loivp13@gmail.com>
2021-08-06 16:59:46 -0700

    set up react and test socket.io

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const app = require("express")();
+
+const server = require("http").createServer(app);
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(socket);
+  console.log("socket is active to be connected");
+  socket.on("chat", (payload) => {
+    console.log("what is payload", payload);
+    io.emit("chat", payload);
+  });
+});
+
+server.listen(5000, () => {
+  console.log("server listening at 5000...");
+});
